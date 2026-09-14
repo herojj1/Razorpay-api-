13a2b2106f5970cfaab46c8cb92426fa000f8f62
herojj1 <monubrahma71@gmail.com>
2026-09-14 16:05:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Card Checkout API - Start Script
-CHECKER_THREADS=${CHECKER_THREADS:-200} \
+CHECKER_THREADS=${CHECKER_THREADS:-60} \
 python3 -u -m uvicorn api_server:app \
   --host 0.0.0.0 \
   --port ${PORT:-8000} \
