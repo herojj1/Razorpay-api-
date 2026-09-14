25f93cd6af0dfd65eaae4684fc333c960dfe2023
herojj1 <monubrahma71@gmail.com>
2026-09-14 18:51:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Card Checkout API - Start Script
-CHECKER_THREADS=${CHECKER_THREADS:-60} \
+CHECKER_THREADS=${CHECKER_THREADS:-10} \
+CHECKER_RETRIES=${CHECKER_RETRIES:-2} \
 python3 -u -m uvicorn api_server:app \
   --host 0.0.0.0 \
   --port ${PORT:-8000} \
   --workers 1 \
-  --timeout-keep-alive 60 \
-  --log-level warning
+  --timeout-keep-alive 120 \
+  --log-level info
