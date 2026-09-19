bf1b3d9211f135f83a03fb2a90dd39b007da6091
herojj1 <monubrahma71@gmail.com>
2026-09-19 23:52:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-CHECKER_THREADS=${CHECKER_THREADS:-10} \
-CHECKER_RETRIES=${CHECKER_RETRIES:-2} \
-python3 -u -m uvicorn api_server:app \
-  --host 0.0.0.0 \
-  --port ${PORT:-8000} \
-  --workers 1 \
-  --timeout-keep-alive 120 \
-  --log-level info
+exec python3 -u bot.py
